c512aa1beb412566fb2f49ef8a9cb8dbf95cf888
Taisuke Fukuno <fukuno@jig.jp>
2023-03-08 10:34:24 +0900

    add afterprocess

--- afterprocess.js
+++ afterprocess.js
@@ -1,14 +1,8 @@
-import { fetchChat } from "https://code4fukui.github.io/ai_chat/fetchChat.js";
 import { writeData } from "https://js.sabae.cc/writeData.js";
-import { sleep } from "https://js.sabae.cc/sleep.js";
 
-const list = JSON.parse(await Deno.readTextFile("kigyoka-koshien-2022.json"));
+const list = JSON.parse(await Deno.readTextFile("kigyoka-koshien-2022-aicomment.json"));
 
 for (const d of list) {
-  console.log(d.title);
-  const res = await fetchChat("あなたなら次のビジネスプランをどのように発展させますか？\n" + d.abstract);
-  console.log(res);
-  d.aicomment = res;
-  await sleep(3000);
+  d.aicomment = d.aicomment.replace("\n\nの利益の取得。", "").trim();
 }
 await writeData("kigyoka-koshien-2022-aicomment", list);
