dd95386d9160d10070114d9a17377d24260d94ab
Taisuke Fukuno <fukuno@jig.jp>
2023-03-09 08:05:16 +0900

    make content

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # kigyoka-koshien-2022
  
+- 起業家甲子園2022オープンデータ ([JSON](kigyoka-koshien-2022.json)/[CSV](kigyoka-koshien-2022.csv)/[CBOR](kigyoka-koshien-2022.cobr))
+- 起業家甲子園2022オープンデータ AIコメント付き ([JSON](kigyoka-koshien-2022-ai.json)/[CSV](kigyoka-koshien-2022-ai.csv)/[CBOR](kigyoka-koshien-2022-ai.cbor))
+- [起業家甲子園2022 ChatGPTくんによるアドバイス](https://code4fukui.github.io/kigyoka-koshien-2022/)
+
+## blog
+
+- https://fukuno.jig.jp/3877
--- makehtml.js
+++ makehtml.js
@@ -1,2 +1,30 @@
 const list = JSON.parse(await Deno.readTextFile("kigyoka-koshien-2022-aicomment.json"));
 
+const title = "起業家甲子園2022 ChatGPTくんによるアドバイス";
+
+const ss = [];
+ss.push(`<!DOCTYPE html><html lang="ja"><head><meta charset="utf-8"><meta name="viewport" content="width=device-width">`);
+ss.push(`<title>${title}</title></head><body><h1>${title}</h1>`);
+ss.push(`<style>
+body {
+  font-family: sans-serif;
+}
+img {
+  width: 100%;
+}
+.abstract, .aicomment {
+  white-space: pre-wrap;
+}
+</style>`);
+for (const d of list) {
+  ss.push(`<h2>${d.title}</h2>`);
+  ss.push(`<img src=${d.img}><br>チーム: ${d.teamname} （${d.school} / ${d.areacontest} - ${d.area}）`);
+  ss.push(`<div class=abstract>`);
+  ss.push(`概要:<br>${d.abstract}`);
+  ss.push(`</div><div class=aicomment>`);
+  ss.push(`ChatGPTくんによるアドバイス<br>${d.aicomment}`);
+  ss.push(`</div>`);
+}
+ss.push("<hr><a href=http://github.com/code4fukui/kigyoka-koshien-2022/>src on GitHub</a>")
+ss.push("</body></html>");
+await Deno.writeTextFile("index.html", ss.join("\n"));
