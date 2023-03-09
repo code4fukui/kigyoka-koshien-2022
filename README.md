bb7c01d0405ce76aece3fbfedde8688a043b846f
Taisuke Fukuno <fukuno@jig.jp>
2023-03-09 15:50:56 +0900

    固有ページ版追加

--- README.md
+++ README.md
@@ -3,6 +3,7 @@
 - 起業家甲子園2022オープンデータ ([JSON](kigyoka-koshien-2022.json)/[CSV](kigyoka-koshien-2022.csv)/[CBOR](kigyoka-koshien-2022.cobr))
 - 起業家甲子園2022オープンデータ AIコメント付き ([JSON](kigyoka-koshien-2022-ai.json)/[CSV](kigyoka-koshien-2022-ai.csv)/[CBOR](kigyoka-koshien-2022-ai.cbor))
 - [起業家甲子園2022 ChatGPTくんによるアドバイス](https://code4fukui.github.io/kigyoka-koshien-2022/)
+- [起業家甲子園2022 ChatGPTくんによるアドバイス 各チーム別](https://code4fukui.github.io/kigyoka-koshien-2022/docs/)
 
 ## blog
 
--- makehtml.js
+++ makehtml.js
@@ -2,10 +2,10 @@ const list = JSON.parse(await Deno.readTextFile("kigyoka-koshien-2022-aicomment.
 
 const title = "起業家甲子園2022 ChatGPTくんによるアドバイス";
 
-const ss = [];
-ss.push(`<!DOCTYPE html><html lang="ja"><head><meta charset="utf-8"><meta name="viewport" content="width=device-width">`);
-ss.push(`<title>${title}</title></head><body><h1>${title}</h1>`);
-ss.push(`<style>
+const doctype = `<!DOCTYPE html><html lang="ja"><head><meta charset="utf-8"><meta name="viewport" content="width=device-width">`;
+const footer = "<hr><a href=http://github.com/code4fukui/kigyoka-koshien-2022/>src on GitHub</a></body></html>";
+
+const style = `<style>
 body {
   font-family: sans-serif;
 }
@@ -16,16 +16,47 @@ img {
   white-space: pre-wrap;
   margin-bottom: 1em;
 }
-</style>`);
-for (const d of list) {
-  ss.push(`<h2>${d.title}</h2>`);
-  ss.push(`<img src=${d.img}><br>チーム: ${d.teamname} （${d.school} / ${d.areacontest} - ${d.area}）`);
+</style>`;
+
+const header = doctype + style;
+
+const d2html = (d, head = "h1") => {
+  const ss = [];
+  ss.push(`<${head}>${d.title}</${head}>`);
+  ss.push(`<img src=${d.img}><br>`);
+  ss.push(`チーム: ${d.teamname} （${d.school} / ${d.areacontest} - ${d.area}）<br>`);
+  ss.push(`ICTメンター: ${d.mentor}<br>`);
   ss.push(`<div class=abstract>`);
   ss.push(`概要:<br>${d.abstract}`);
   ss.push(`</div><div class=aicomment>`);
   ss.push(`ChatGPTくんによるアドバイス<br>${d.aicomment}`);
   ss.push(`</div>`);
+  return ss.join("\n");
+};
+
+const ss = [];
+ss.push(header);
+ss.push(`<title>${title}</title></head><body><h1>${title}</h1>`);
+ss.push(style);
+for (const d of list) {
+  ss.push(d2html(d, "h2"));
 }
-ss.push("<hr><a href=http://github.com/code4fukui/kigyoka-koshien-2022/>src on GitHub</a>")
-ss.push("</body></html>");
-await Deno.writeTextFile("index.html", ss.join("\n"));
+ss.push(footer);
+await Deno.writeTextFile("all.html", ss.join("\n"));
+
+await Deno.mkdir("docs", { recursive: true });
+const html = [];
+html.push(header);
+html.push(`<title>${title}</title></head><body><h1>${title}</h1>`);
+for (const d of list) {
+  const ss = [];
+  ss.push(header);
+  ss.push(`<title>${d.title}</title></head><body>`);
+  ss.push(d2html(d, "h1"));
+  ss.push(footer);
+  const name = d.img.substring(d.img.lastIndexOf("/") + 1, d.img.lastIndexOf(".")) + "html";
+  await Deno.writeTextFile(`docs/${name}.html`, ss.join("\n"));
+  html.push(`<a href=${name}.html>${d.title} - ${d.teamname}</a><br>`);
+}
+html.push(footer);
+await Deno.writeTextFile("docs/index.html", html.join("\n"));
